59d33d58c0b588a62330b354316151726655b28d
Gregory <parfeniuk33@gmail.com>
2022-10-27 21:59:04 +0300

    I have only C drive, so for my PC it works properly, but sometimes prints that Path isn't correts. And of cource it can count only files with pen access

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class Main {
+import java.io.*;
+import java.nio.file.AccessDeniedException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        final Path path = Path.of("c:\\windows").getRoot();
+        final List<Path> allFiles = getAllFiles(path);
+        if (allFiles != null){
+            for (final Path filepath : allFiles){
+                System.out.println(filepath);
+            }
+            System.out.printf("There was %d available files", allFiles.size());
+
+        }
+    }
+
+    private static List<Path> getAllFiles(final Path startPath){
+        List<Path> lst;
+        try (final DirectoryStream<Path> directoryStream = Files.newDirectoryStream(startPath)){
+            lst = new ArrayList<>();
+            for (final Path path : directoryStream){
+                if (Files.isRegularFile(path)){
+                    lst.add(path);
+                } else {
+                    final List<Path> tempList = getAllFiles(path);
+                    if (tempList != null){
+                        lst.addAll(tempList);
+                    }
+                }
+            }
+            return lst;
+        } catch (AccessDeniedException e) {
+        } catch (IOException e) {
+            System.out.println("Path isn't correct");
+        }
+        return null;
+    }
 }
